cc5187be1d48c4d55ebbefb1c33c5c0fd371441f
KSM1598 <45555721+KSM1598@users.noreply.github.com>
2019-01-23 22:56:04 +0530

    updated block quotes

--- newsdataa.py
+++ newsdataa.py
@@ -2,31 +2,31 @@
 import psycopg2
 
 tc1 = ("1. What are the most popular three articles of all time?")
-q1 = ("create view articles_view as select articles.title, "
-      " count(*) as total_views "
-      " from articles inner join log "
-      " on concat('/article/', articles.slug) = log.path "
-      "where log.status like '%200%' group by articles.title"
-      " order by total_views desc limit 3;")
+q1 = ('''create view articles_view as select articles.title,
+         count(*) as total_views
+         from articles inner join log
+         on concat('/article/', articles.slug) = log.path
+         where log.status like '%200%' group by articles.title
+         order by total_views desc limit 3;''')
 
 tc2 = ("2. Who are the most popular article authors of all time?")
-q2 = ("create view authors_view as select authors.name, "
-      "count(*) as total_views "
-      " from authors inner join articles "
-      " on authors.id=articles.author inner join log "
-      " on concat('/article/', articles.slug) = log.path "
-      " group by authors.name order by total_views desc;")
+q2 = ('''create view authors_view as select authors.name,
+         count(*) as total_views
+         from authors inner join articles
+         on authors.id=articles.author inner join log
+         on concat('/article/', articles.slug) = log.path
+         group by authors.name order by total_views desc;''')
 
 tc3 = ("3. On which days did more than 1% of the requests lead to errors?")
-q3 = ("create view log_errors as "
-      "select Date,Total_records,Errors,"
-      "(Errors::float*100)/Total_records::float as Error_Percentage "
-      " from (select time::timestamp::date as Date, "
-      "count(status) as Total_records,"
-      "sum(case when status like '%404%' then 1 else 0 end) as Errors "
-      "from log group by time::timestamp::date) as result "
-      "where (Errors::float*100)/Total_records::float > 1.0 "
-      "order by Error_Percentage desc;")
+q3 = ('''create view log_errors as
+      select Date,Total_records,Errors,
+      (Errors::float*100)/Total_records::float as Error_Percentage
+      from (select time::timestamp::date as Date,
+      count(status) as Total_records,
+      sum(case when status like '%404%' then 1 else 0 end) as Errors
+      from log group by time::timestamp::date) as result
+      where (Errors::float*100)/Total_records::float > 1.0
+      order by Error_Percentage desc;''')
 
 
 def dbconnect(database_name="news"):
@@ -42,7 +42,7 @@ def dbconnect(database_name="news"):
 
 def exec_query1(q):
     cnxn, cursor = dbconnect()
-    cursor.execute(q)
+#   cursor.execute(q)
     qt = ("select * from articles_view;")
     cursor.execute(qt)
     cnxn.commit()
@@ -52,7 +52,7 @@ def exec_query1(q):
 
 def exec_query2(q):
     cnxn, cursor = dbconnect()
-    cursor.execute(q)
+#   cursor.execute(q)
     qt = ("select * from authors_view;")
     cursor.execute(qt)
     cnxn.commit()
@@ -62,7 +62,7 @@ def exec_query2(q):
 
 def exec_query3(q):
     cnxn, cursor = dbconnect()
-    cursor.execute(q)
+#   cursor.execute(q)
     qt = ("select * from log_errors;")
     cursor.execute(qt)
     cnxn.commit()
